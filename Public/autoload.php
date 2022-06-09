f8be0a2b780e7b5045b0b974faa23abb642c7a75
Gawailan <donatian.pauwels@gmail.com>
2022-06-09 14:43:08 +0200

    Ajout class Controller abstract

---
+++ Public/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+//Rechercher pour totalement comprendre.
+
+spl_autoload_register ( function ($class) 
+{
+	if (strpos ( $class, "DAO") !== false) {
+		require_once("models/dao/{$class}.php");
+	} else if (strpos ( $class, "Controller") !== false) {
+        require_once("controllers/{$class}.php");
+    } else {
+		require_once("models/entities/{$class}.php");
+	}
+}
+);
+
+?>
--- Public/index.php
+++ Public/index.php
@@ -1,3 +1,10 @@
+<?php
+require_once("autoload.php");
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
