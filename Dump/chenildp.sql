93bd86630de1341d8e1ca9196f8fe910ed97a53a
Gawailan <donatian.pauwels@gmail.com>
2022-06-18 22:19:37 +0200

    Mise à jour du dump DB

--- Dump/chenildp.sql
+++ Dump/chenildp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 13 juin 2022 à 22:21
+-- Généré le : sam. 18 juin 2022 à 20:18
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -18,8 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `chenil`
+-- Base de données : `chenildp`
 --
+CREATE DATABASE IF NOT EXISTS `chenildp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `chenildp`;
 
 -- --------------------------------------------------------
 
@@ -31,6 +33,7 @@ DROP TABLE IF EXISTS `t_animals`;
 CREATE TABLE IF NOT EXISTS `t_animals` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name_animal` varchar(20) NOT NULL,
+  `specie_animal` varchar(20) NOT NULL,
   `gender_animal` tinyint(1) NOT NULL,
   `bday_animal` date NOT NULL,
   `sterilized_animal` tinyint(1) NOT NULL,
@@ -39,16 +42,14 @@ CREATE TABLE IF NOT EXISTS `t_animals` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `microship_animal` (`microship_animal`),
   KEY `fk_animals_persons` (`fk_id_person`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `t_animals`
 --
 
-INSERT INTO `t_animals` (`id`, `name_animal`, `gender_animal`, `bday_animal`, `sterilized_animal`, `microship_animal`, `fk_id_person`) VALUES
-(5, 'Coton', 1, '2021-03-12', 1, 1111111111111112, 18),
-(6, 'Cerise', 0, '2002-06-19', 1, 111111111111113, 19),
-(8, 'fsesfse', 1, '2022-06-06', 1, 156155161651615, 18);
+INSERT INTO `t_animals` (`id`, `name_animal`, `specie_animal`, `gender_animal`, `bday_animal`, `sterilized_animal`, `microship_animal`, `fk_id_person`) VALUES
+(35, 'Coton', 'Canin', 1, '2021-03-12', 1, 101010101010101, 53);
 
 -- --------------------------------------------------------
 
@@ -63,15 +64,16 @@ CREATE TABLE IF NOT EXISTS `t_boardings` (
   `endDate_boarding` date NOT NULL,
   `fk_id_animal` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_boarding_animals` (`fk_id_animal`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  KEY `fk_boardings_animals` (`fk_id_animal`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `t_boardings`
 --
 
 INSERT INTO `t_boardings` (`id`, `startDate_boarding`, `endDate_boarding`, `fk_id_animal`) VALUES
-(5, '2022-06-06', '2022-08-01', 5);
+(23, '2022-02-01', '2022-02-15', 35),
+(24, '2022-08-05', '2022-09-09', 35);
 
 -- --------------------------------------------------------
 
@@ -88,16 +90,15 @@ CREATE TABLE IF NOT EXISTS `t_persons` (
   `email_person` varchar(120) NOT NULL,
   `phone_person` varchar(12) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `t_persons`
 --
 
 INSERT INTO `t_persons` (`id`, `name_person`, `firstname_person`, `bday_person`, `email_person`, `phone_person`) VALUES
-(18, 'Pauwels', 'Donatian', '1992-09-16', 'donatian.pauwels@gmail.com', '0497089411'),
-(19, 'Perroncel', 'Gaelle', '2002-06-19', 'gaelle.perroncel@gmail.com', '0497089411'),
-(31, 'Jean-francois', 'Pierart', '1991-08-20', 'fdssdfs@fdfsff.com', '415451415');
+(52, 'Pauwels', 'Donatian', '1992-09-15', 'donatian.pauwels@gmail.com', '0000000000'),
+(53, 'Delbar', 'Benjamin', '2022-06-18', 'benjamin.delbar@ifosup.wavre.be', '0000000000');
 
 --
 -- Contraintes pour les tables déchargées
@@ -113,7 +114,7 @@ ALTER TABLE `t_animals`
 -- Contraintes pour la table `t_boardings`
 --
 ALTER TABLE `t_boardings`
-  ADD CONSTRAINT `fk_boarding_animals` FOREIGN KEY (`fk_id_animal`) REFERENCES `t_animals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_boardings_animals` FOREIGN KEY (`fk_id_animal`) REFERENCES `t_animals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
